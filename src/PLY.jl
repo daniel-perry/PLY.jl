6c56a91dd19fd99b6173bb406263b1ee7a749a59
Danny Perry <dperry@sci.utah.edu>
2015-10-28 11:58:14 -0600

    adding install command for headers.

--- src/PLY.jl
+++ src/PLY.jl
@@ -1,15 +1,26 @@
 #module PLY
 
-function plyopen(fn)
+function plyopen(fn::ASCIIString)
 	nelems = zeros(Int32,1)
-	elem_names = [ ["        ",], ["      ",], ["     ",] ]
+	elem_names = Array{ASCIIString,1}[]
+	push!(elem_names, ASCIIString[])
+	push!(elem_names, ASCIIString[])
+	push!(elem_names, ASCIIString[])
+	println(typeof(elem_names))
 	file_type = zeros(Int32,1)
 	version = [0.0f0]
-	fh = ccall( (:ply_open_for_reading, :libply), 
+	fh = ccall( (:ply_open_for_reading, "libply"), 
 	            Ptr{Void}, 
 							(Ptr{Uint8}, Ptr{Int32}, Ptr{Ptr{Ptr{Uint8}}}, Ptr{Int32}, Ptr{Float32}),
 							fn,nelems,elem_names,file_type,version)
 	return fh,nelems,elem_names,file_type,version
+end
+
+function plyclose(handle::Ptr{Void})
+	ccall( (:ply_close, "libply"), 
+	        Void,
+					(Ptr{Void},),
+					handle)
 end
 
 #end # module PLY
